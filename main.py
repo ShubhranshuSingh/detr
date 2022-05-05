7fd2aeaf361290c435f461b7a3566a191680215d
Ubuntu <ubuntu@ip-172-31-41-141.us-east-2.compute.internal>
2022-05-05 20:56:41 +0000

    Tune Encoder

--- main.py
+++ main.py
@@ -211,7 +211,7 @@ def main(args):
             continue
     model.load_state_dict(own_state)
     for param in model.named_parameters():
-        if param[0].find('resizer') == -1:
+        if param[0].find('encoder') == -1:
             param[1].requires_grad = False
     model.query_embed.weight.requires_grad = True
 
